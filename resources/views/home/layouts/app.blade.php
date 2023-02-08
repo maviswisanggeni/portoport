70b0fc8e006b3bd618e0da208859d90fbfce255d
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2023-02-08 16:32:21 +0700

    add: [MW] add x-icon

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home - Portoport</title>
+    <title>Home | Portoport</title>
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/card-product.css">
     <link rel="stylesheet" href="css/products.css">
+    {{-- add icon --}}
+    <link rel="shortcut icon" href="icons/x-icon.svg" type="image/x-icon">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
     <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
