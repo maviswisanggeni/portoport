39bcfa3b4ee57da5deef9937ec4f8bd3228a3853
Ario <@ariogalang7@gmail.com>
2023-02-22 13:50:40 +0700

    add: [AP] add go pro page

---
+++ resources/views/go-pro.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/go-pro.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+</head>
+<body>
+     <div class="container">
+        <div class="title">
+            <h1>Show your work to the <span>world</span> with</h1>
+            <h2>Port<span>o</span>Port Pro, and ProPlus!</h2>
+            <h3>With Pro you can show your 5 best product at PortoPort, but with Pro Plus? sure you can show more with no limitation!</h3>
+        </div>
+        <div class="image-3d">
+            <img src="{{ asset('images/boy.png') }}">
+        </div>
+     </div>
+
+    <div class="title2">
+      <div class="circleBase circle1"></div>
+      <div class="circleBase circle2"></div>
+      <h1><span>Things you</span> can get with Pro and Pro Plus</h1>
+        </div>
+
+    <div class="info">
+        <div class="title3">
+        <h1>Better way to introduce your self to clients</h1>
+        <h3>Exclusive to Pro and Pro Plus subscribers, Pitch gives you a whole new way to introduce yourself to prospective clients right from your Portolians profile with your very own personalized video.</h3>
+        </div>
+
+    <div class="image-3d2">
+            <img src="{{ asset('images/person2.png') }}">
+        </div>
+        </div>
+
+        <div class="product">
+        <div class="wave upper">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000" fill-opacity="1" d="M0,96L48,112C96,128,192,160,288,149.3C384,139,480,85,576,69.3C672,53,768,75,864,101.3C960,128,1056,160,1152,144C1248,128,1344,64,1392,32L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        </div>
+        
+        
+        <div class="wave">
+            <h1>Be prepared there will be a lot of people see your product!</h1>
+            <h2>Get a lot of fans and client with your creatifity and your product with PortoPort right now!</h2>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000" fill-opacity="1" d="M0,96L48,112C96,128,192,160,288,149.3C384,139,480,85,576,69.3C672,53,768,75,864,101.3C960,128,1056,160,1152,144C1248,128,1344,64,1392,32L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+        </div>
+
+        <div class="imageexample1">
+         <img src="{{ asset('images/Rectangle 118.png') }}">
+         </div>
+        
+        <div class="imageexample2">
+         <img src="{{ asset('images/Rectangle 76.png') }}">
+         </div>
+
+        <div class="imageexample3">
+         <img src="{{ asset('images/Rectangle 121.png') }}">
+         </div>
+
+        <div class="imageexample4">
+         <img src="{{ asset('images/Rectangle 120(1).png') }}">
+         </div>
+
+        <div class="pricing">
+                <h1>P<span>o</span>rtoPort Packages & Pricing</h1>
+                <h2>Perfect for showing your talent to people all around the globe 2D Arts, or even on aplication</h2>        
+            </div>
+
+        <div class="payswitchbtn">        
+        <div class="rectangle">
+                <h1>Pay Yearly</h1>
+        </div>
+
+        <div class="rectangle2">
+                <h1>Pay Monthly</h1>
+        </div>
+        </div>
+        
+        <div class="package-container">
+            <div class="package-box">
+                <h1 class="package-title">Basic</h1>
+                <h2 class="package-description">The essentials to get your talent going</h2>
+                <h1 class="package-price">FREE</h1>
+                <div class="btn-package">
+                    <h3>Default</h3>
+                </div>
+                <ul class="package-benefit">
+                    <li>All essential features</li>
+                    <li>Able to upload one image/project</li>
+                    <li>Great security and support</li>
+                </ul>
+            </div>
+
+            <div class="package-box">
+                <h1 class="package-title">Pro</h1>
+                <h2 class="package-description">Powerful tools to publicate your talent</h2>
+                <h1 class="package-price">Rp 50k/Month</h1>
+                <div class="btn-package">
+                    <h3>Get started</h3>
+                </div>
+                <ul class="package-benefit">
+                    <li>Everything in Basic</li>
+                    <li>Upload up to five image/project</li>
+                    <li>Get your talent to spread world wide</li>
+                </ul>
+            </div>
+
+            <div class="package-box">
+                <h1 class="package-title">Pro+</h1>
+                <h2 class="package-description">All of our feature with advanced controll and support</h2>
+                <h1 class="package-price">Rp 150k/Month</h1>
+                <div class="btn-package">
+                    <h3>Get started</h3>
+                </div>
+                <ul class="package-benefit">
+                    <li>Everything in pro</li>
+                    <li>Able to upload limitless image</li>
+                    <li>VIP Support</li>
+                </ul>
+            </div>
+        </div>
+</body>
+</html>
