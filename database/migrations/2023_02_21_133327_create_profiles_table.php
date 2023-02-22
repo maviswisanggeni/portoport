000e2503f3779c342fd288260bb3e257486d6126
Arfa <arfaazizan.naufal@gmail.com>
2023-02-22 13:24:08 +0700

    Feat:[AN] Database Table

---
+++ database/migrations/2023_02_21_133327_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('profile_name');
+            $table->string('phone_number');
+            $table->foreignId('account_id');
+            $table->foreignId('like_id');
+            $table->foreignId('follow_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
